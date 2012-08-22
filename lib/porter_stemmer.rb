26ed831e94adf8739a37aaacc99918e5d6808ce1
Kevin Wellenzohn <kevin.wellenzohn@stud-inf.unibz.it>
2012-08-22 08:16:45 +0200

    Changed the way how ruby files are loaded in subdirectories

--- lib/porter_stemmer.rb
+++ lib/porter_stemmer.rb
@@ -2,6 +2,7 @@ module PorterStemmer
 end
 
 # require all ruby files in the porter_stemmer directory
-Dir[File.dirname(__FILE__) + '/porter_stemmer/*.rb'].each do |file| 
-  require file 
+current_path = File.expand_path(File.dirname(__FILE__))
+Dir[current_path + '/lib/**/*.rb'].uniq.each do |file|
+  require file
 end
